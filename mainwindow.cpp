27cd837d7131c3e1b52cf729075a69aa6593805e
Tobias Klenke <tobias.klenke@htp-tel.de>
2023-08-05 16:44:45 +0200

    Reworked documentation

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,39 +1,50 @@
 /****************************************************************************************************************************************************
- * Includes
+ * INCLUDES                                                                                                                                         *
  ****************************************************************************************************************************************************/
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
 /****************************************************************************************************************************************************
- * Definition of Public Functions
+ * DEFINITION OF PUBLIC FUNCTIONS                                                                                                                   *
  ****************************************************************************************************************************************************/
 
-/*!
- * \brief This function is the constructor of the class MainWindow.
- */
+/*!**************************************************************************************************************************************************
+ * \brief   This function is the constructor of the class MainWindow.                                                                               *
+ *                                                                                                                                                  *
+ * \details This function sets up the user interface of the class MainWindow and connects the signals and slots of the main window actions.         *
+ *          Afterwards, it shows the player window in full screen mode and moves it to the secondary screen.                                        *
+ *                                                                                                                                                  *
+ * \param   parent                        Parent of the class MainWindow                                                                            *
+ *                                                                                                                                                  *
+ * \return  This function does not have any return value.                                                                                           *
+ ****************************************************************************************************************************************************/
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     pUserInterface(new Ui::MainWindow),
-    pDialog_NewBattleMap(NULL),
+    pDialog_NewBattleMap(nullptr),
     pPlayerScreenWindow(new QGraphicsView)
 {
     qDebug() << "..." << __func__;
 
     pUserInterface->setupUi(this);
 
-    /* connections */
+    /* connect signals and slots of the main window actions */
     connect(pUserInterface->Action_NewBattleMap, SIGNAL(triggered()), this, SLOT(open_Dialog_NewBattleMap()));
     connect(pUserInterface->Action_Quit, SIGNAL(triggered()), QApplication::instance(), SLOT(quit()));
 
-    //TODO: show player window
-    //pPlayerScreenWindow->showFullScreen();
-    //pPlayerScreenWindow->move(1920, 0);
+    /* show the player window in full screen mode and move it to the secondary screen */
+    pPlayerScreenWindow->showFullScreen();
+    pPlayerScreenWindow->move(MASTER_SCREEN_RESOLUTION.width(), 0);
 }
 
-/*!
- * \brief This function is the destructor of the class MainWindow.
- */
+/*!**************************************************************************************************************************************************
+ * \brief   This function is the destructor of the class MainWindow.                                                                                *
+ *                                                                                                                                                  *
+ * \details This function deletes the object pointed to by pUserInterface.                                                                          *
+ *                                                                                                                                                  *
+ * \return  This function does not have any return value.                                                                                           *
+ ****************************************************************************************************************************************************/
 MainWindow::~MainWindow()
 {
     qDebug() << "..." << __func__;
@@ -42,66 +53,60 @@ MainWindow::~MainWindow()
 }
 
 /****************************************************************************************************************************************************
- * Definition of Protected Functions
+ * DEFINITION OF PROTECTED FUNCTIONS                                                                                                                *
  ****************************************************************************************************************************************************/
 
 /* - */
 
 /****************************************************************************************************************************************************
- * Definition of Private Slot Functions
+ * DEFINITION OF PRIVATE SLOT FUNCTIONS                                                                                                             *
  ****************************************************************************************************************************************************/
 
-/*!
- * \brief This function opens the dialog Dialog_NewBattleMap.
- */
+/*!**************************************************************************************************************************************************
+ * \brief   This function handles the action Action_NewBattleMap and opens the dialog Dialog_NewBattleMap.                                          *
+ *                                                                                                                                                  *
+ * \details This function creates a dialog Dialog_NewBattleMap and connects the signals and slots of the dialog button box actions of the dialog    *
+ *          Dialog_NewBattleMap. Afterwards, it opens the dialog Dialog_NewBattleMap.                                                               *
+ *                                                                                                                                                  *
+ * \return  This function does not have any return value.                                                                                           *
+ ****************************************************************************************************************************************************/
 void MainWindow::open_Dialog_NewBattleMap()
 {
     qDebug() << "..." << __func__;
 
     pDialog_NewBattleMap = new Dialog_NewBattleMap(this);
 
-    /* connections */
+    /* connect signals and slots of the dialog button box actions of the dialog */
     connect(pDialog_NewBattleMap, SIGNAL(accepted()), this, SLOT(accepted_Dialog_NewBattleMap()));
     connect(pDialog_NewBattleMap, SIGNAL(rejected()), this, SLOT(rejected_Dialog_NewBattleMap()));
 
-    pDialog_NewBattleMap->setWindowModality(Qt::WindowModal);
     pDialog_NewBattleMap->open();
 }
 
-/*!
- * \brief This function handels the acceptance of the dialog Dialog_NewBattleMap.
- */
+/*!**************************************************************************************************************************************************
+ * \brief   This function handles the acceptance of the dialog Dialog_NewBattleMap.                                                                 *
+ *                                                                                                                                                  *
+ * \details This function stores the Battle Map object created via the dialog Dialog_NewBattleMap. Afterwards, it deletes the object pointed to by  *
+ *          pDialog_NewBattleMap.                                                                                                                   *
+ *                                                                                                                                                  *
+ * \return  This function does not have any return value.                                                                                           *
+ ****************************************************************************************************************************************************/
 void MainWindow::accepted_Dialog_NewBattleMap()
 {
     qDebug() << "..." << __func__;
-
-    //TODO: remove
-    qDebug() << pDialog_NewBattleMap->getBattleMapPixmap();
-    qDebug() << pDialog_NewBattleMap->getBattleMap()->getNumberRows();
-    qDebug() << pDialog_NewBattleMap->getBattleMap()->getNumberColumns();
-
-//    pDialog_NewBattleMap->getBattleMapPixmap().save("test/test.jpg");
-
-//    quint32 numberRows = pDialog_NewBattleMap->getBattleMap()->getNumberRows();
-//    quint32 numberColumns = pDialog_NewBattleMap->getBattleMap()->getNumberColumns();
-
-//    for (quint32 rowIdx = 0U; rowIdx < numberRows; rowIdx++)
-//    {
-//        for (quint32 columnIdx = 0U; columnIdx < numberColumns; columnIdx++)
-//        {
-//            pDialog_NewBattleMap->getBattleMap()->getIndexedBattleMapSquarePixmap(rowIdx, columnIdx)->pixmap().save("test/test_" + QString::number(rowIdx) + "_" + QString::number(columnIdx) +".jpg");
-//        }
-//    }
 
     //TODO: Implement storation of data before deleting pDialog_NewBattleMap
 
     delete pDialog_NewBattleMap;
 }
 
-
-/*!
- * \brief This function handels the rejection of the dialog Dialog_NewBattleMap.
- */
+/*!**************************************************************************************************************************************************
+ * \brief   This function handles the rejection of the dialog Dialog_NewBattleMap.                                                                  *
+ *                                                                                                                                                  *
+ * \details This function deletes the object pointed to by pDialog_NewBattleMap.                                                                    *
+ *                                                                                                                                                  *
+ * \return  This function does not have any return value.                                                                                           *
+ ****************************************************************************************************************************************************/
 void MainWindow::rejected_Dialog_NewBattleMap()
 {
     qDebug() << "..." << __func__;
@@ -110,7 +115,7 @@ void MainWindow::rejected_Dialog_NewBattleMap()
 }
 
 /****************************************************************************************************************************************************
- * Definition of Private Functions
+ * DEFINITION OF PRIVATE FUNCTIONS                                                                                                                  *
  ****************************************************************************************************************************************************/
 
 /* - */
