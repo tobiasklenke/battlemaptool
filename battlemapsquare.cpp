c521110031727f99ae4e9e13b423c53ae33de69e
Tobias Klenke <tobias.klenke@htp-tel.de>
2023-08-22 17:39:09 +0200

    Reworked documentation

--- battlemapsquare.cpp
+++ battlemapsquare.cpp
@@ -8,54 +8,36 @@
  * DEFINITION OF PUBLIC FUNCTIONS                                                                                                                   *
  ****************************************************************************************************************************************************/
 
-/*!**************************************************************************************************************************************************
- * \brief   This function is the constructor of the class BattleMapSquare.                                                                          *
- *                                                                                                                                                  *
- * \details This function initializes the Battle Map square with the image of an empty Battle Map square.                                           *
- *                                                                                                                                                  *
- * \return  This function does not have any return value.                                                                                           *
- ****************************************************************************************************************************************************/
+/*!
+ * \brief This function is the constructor of the class BattleMapSquare.
+ */
 BattleMapSquare::BattleMapSquare() :
     pBattleMapSquarePixMap(new QGraphicsPixmapItem())
 {
     /* initialize Battle Map square with image of empty Battle Map square */
-    pBattleMapSquarePixMap->setPixmap(QPixmap::fromImage(QImage((EMPTY_BATTLEMAPSQUARE_SOURCE))));
+    pBattleMapSquarePixMap->setPixmap(QPixmap::fromImage(QImage((EMPTYBATTLEMAPSQUARE_SOURCE))));
     pBattleMapSquarePixMap->setPixmap(pBattleMapSquarePixMap->pixmap().scaled(QSize(BATTLEMAPSQUARE_SIZE, BATTLEMAPSQUARE_SIZE)));
 }
 
-/*!**************************************************************************************************************************************************
- * \brief   This function is the destructor of the class BattleMapSquare.                                                                           *
- *                                                                                                                                                  *
- * \details This function deletes the object pointed to by pBattleMapSquarePixMap.                                                                  *
- *                                                                                                                                                  *
- * \return  This function does not have any return value.                                                                                           *
- ****************************************************************************************************************************************************/
+/*!
+ * \brief This function is the destructor of the class BattleMapSquare.
+ */
 BattleMapSquare::~BattleMapSquare()
 {
     delete pBattleMapSquarePixMap;
 }
 
-/*!**************************************************************************************************************************************************
- * \brief   This function returns the value of the member variable pBattleMapSquarePixMap.                                                          *
- *                                                                                                                                                  *
- * \details -                                                                                                                                       *
- *                                                                                                                                                  *
- * \return  This function returns the value of the member variable pBattleMapSquarePixMap.                                                          *
- ****************************************************************************************************************************************************/
+/*!
+ * \brief This function returns the value of the member variable pBattleMapSquarePixMap.
+ */
 QGraphicsPixmapItem *BattleMapSquare::getBattleMapSquarePixMap() const
 {
     return pBattleMapSquarePixMap;
 }
 
-/*!**************************************************************************************************************************************************
- * \brief   This function sets the value of the member variable pBattleMapSquarePixMap.                                                             *
- *                                                                                                                                                  *
- * \details -                                                                                                                                       *
- *                                                                                                                                                  *
- * \param   battleMapSquarePixMap         Pixmap of the Battle Map square                                                                           *
- *                                                                                                                                                  *
- * \return  This function does not have any return value.                                                                                           *
- ****************************************************************************************************************************************************/
+/*!
+ * \brief This function sets the value of the member variable pBattleMapSquarePixMap.
+ */
 void BattleMapSquare::setBattleMapSquarePixMap(QGraphicsPixmapItem *battleMapSquarePixMap)
 {
     pBattleMapSquarePixMap = battleMapSquarePixMap;
--- screencalc.cpp
+++ screencalc.cpp
@@ -20,65 +20,33 @@
  * LOCAL FUNCTIONS                                                                                                                                  *
  ****************************************************************************************************************************************************/
 
-/*!**************************************************************************************************************************************************
- * \brief   This function calculates the screen width in inches.                                                                                    *
- *                                                                                                                                                  *
- * \details -                                                                                                                                       *
- *                                                                                                                                                  *
- * \param   screenDiagonal                Screen diagonal in inches                                                                                 *
- * \param   resolutionWidth               Screen width in pixels                                                                                    *
- * \param   resolutionHeight              Screen height in pixels                                                                                   *
- *                                                                                                                                                  *
- * \return  This function returns the calculated screen width in inches.                                                                            *
- ****************************************************************************************************************************************************/
+/*!
+ * \brief This function calculates the screen width in inches.
+ */
 qreal calcScreenWidthInInches(qreal screenDiagonal, quint32 resolutionWidth, quint32 resolutionHeight)
 {
     return qSqrt(qPow(screenDiagonal, 2) / (1 + qPow(static_cast<qreal>(resolutionWidth) / static_cast<qreal>(resolutionHeight), 2)));
 }
 
-/*!**************************************************************************************************************************************************
- * \brief   This function calculates the screen width in centimeters.                                                                               *
- *                                                                                                                                                  *
- * \details -                                                                                                                                       *
- *                                                                                                                                                  *
- * \param   screenDiagonal                Screen diagonal in inches                                                                                 *
- * \param   resolutionWidth               Screen width in pixels                                                                                    *
- * \param   resolutionHeight              Screen height in pixels                                                                                   *
- *                                                                                                                                                  *
- * \return  This function returns the calculated screen width in centimeters.                                                                       *
- ****************************************************************************************************************************************************/
+/*!
+ * \brief This function calculates the screen width in centimeters.
+ */
 qreal calcScreenWidthInCentimeters(qreal screenDiagonal, quint32 resolutionWidth, quint32 resolutionHeight)
 {
     return calcScreenWidthInInches(screenDiagonal, resolutionWidth, resolutionHeight) * INCH_IN_CENTIMETERS;
 }
 
-/*!**************************************************************************************************************************************************
- * \brief   This function calculates the screen height in inches.                                                                                   *
- *                                                                                                                                                  *
- * \details -                                                                                                                                       *
- *                                                                                                                                                  *
- * \param   screenDiagonal                Screen diagonal in inches                                                                                 *
- * \param   resolutionWidth               Screen width in pixels                                                                                    *
- * \param   resolutionHeight              Screen height in pixels                                                                                   *
- *                                                                                                                                                  *
- * \return  This function returns the calculated screen height in inches.                                                                           *
- ****************************************************************************************************************************************************/
+/*!
+ * \brief This function calculates the screen height in inches.
+ */
 qreal calcScreenHeightInInches(qreal screenDiagonal, quint32 resolutionWidth, quint32 resolutionHeight)
 {
     return qSqrt(qPow(screenDiagonal, 2) / (1 + qPow(static_cast<qreal>(resolutionHeight) / static_cast<qreal>(resolutionWidth), 2)));
 }
 
-/*!**************************************************************************************************************************************************
- * \brief   This function calculates the screen height in centimeters.                                                                              *
- *                                                                                                                                                  *
- * \details -                                                                                                                                       *
- *                                                                                                                                                  *
- * \param   screenDiagonal                Screen diagonal in inches                                                                                 *
- * \param   resolutionWidth               Screen width in pixels                                                                                    *
- * \param   resolutionHeight              Screen height in pixels                                                                                   *
- *                                                                                                                                                  *
- * \return  This function returns the calculated screen height in centimeters.                                                                      *
- ****************************************************************************************************************************************************/
+/*!
+ * \brief This function calculates the screen height in centimeters.
+ */
 qreal calcScreenHeightInCentimeters(qreal screenDiagonal, quint32 resolutionWidth, quint32 resolutionHeight)
 {
     return calcScreenHeightInInches(screenDiagonal, resolutionWidth, resolutionHeight) * INCH_IN_CENTIMETERS;
