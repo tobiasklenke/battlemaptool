c235b73cfc100ba7f3ed4b5b31b9ccf07d75c086
Tobias Klenke <tobias.klenke@htp-tel.de>
2023-10-30 13:15:38 +0100

    Initial version

---
+++ customgraphicspixmapitem.h
@@ -0,0 +1,64 @@
+#ifndef CUSTOMGRAPHICSPIXMAPITEM_H
+#define CUSTOMGRAPHICSPIXMAPITEM_H
+
+/****************************************************************************************************************************************************
+ * INCLUDES                                                                                                                                         *
+ ****************************************************************************************************************************************************/
+
+#include <QObject>
+#include <QGraphicsPixmapItem>
+
+/****************************************************************************************************************************************************
+ * CLASS DECLARATION                                                                                                                                *
+ ****************************************************************************************************************************************************/
+
+/*!
+ * \brief This class implements the custom functionality of a transparent graphics pixmap item.
+ */
+class CustomGraphicsPixmapItem : public QObject, public QGraphicsPixmapItem
+{
+    Q_OBJECT
+    Q_PROPERTY(qreal opacity READ opacity WRITE setOpacity)
+
+public:
+
+    /*! *********************************************************************************************************************************************
+     * \brief   This function is the constructor of the class CustomGraphicsPixmapItem.                                                             *
+     *                                                                                                                                              *
+     * \details -                                                                                                                                   *
+     *                                                                                                                                              *
+     * \return  This function does not have any return value.                                                                                       *
+     ************************************************************************************************************************************************/
+    CustomGraphicsPixmapItem();
+
+    /*! *********************************************************************************************************************************************
+     * \brief   This function is the constructor of the class CustomGraphicsPixmapItem, using pixmap as the default pixmap.                         *
+     *                                                                                                                                              *
+     * \details -                                                                                                                                   *
+     *                                                                                                                                              *
+     * \param   pixmap                        Pixmap of the graphics pixmap item                                                                    *
+     *                                                                                                                                              *
+     * \return  This function does not have any return value.                                                                                       *
+     ************************************************************************************************************************************************/
+    CustomGraphicsPixmapItem(QPixmap pixmap);
+
+    /*! *********************************************************************************************************************************************
+     * \brief   This function is the destructor of the class CustomGraphicsPixmapItem.                                                              *
+     *                                                                                                                                              *
+     * \details -                                                                                                                                   *
+     *                                                                                                                                              *
+     * \return  This function does not have any return value.                                                                                       *
+     ************************************************************************************************************************************************/
+    ~CustomGraphicsPixmapItem();
+
+protected: /* - */
+
+signals: /* - */
+
+private slots: /* - */
+
+private: /* - */
+
+};
+
+#endif // CUSTOMGRAPHICSPIXMAPITEM_H
