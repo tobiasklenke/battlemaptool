eee5344c4132f22e05af7dd0e683eace25709494
Tobias Klenke <tobias.klenke@htp-tel.de>
2024-03-30 18:43:54 +0100

    Initial version

---
+++ undocommandpaste.cpp
@@ -0,0 +1,94 @@
+/****************************************************************************************************************************************************
+ * INCLUDES                                                                                                                                         *
+ ****************************************************************************************************************************************************/
+
+#include "undocommandpaste.h"
+
+/****************************************************************************************************************************************************
+ * DEFINITION OF PUBLIC FUNCTIONS                                                                                                                   *
+ ****************************************************************************************************************************************************/
+
+/*!
+ * \brief This function is the constructor of the class UndoCommandPaste.
+ */
+UndoCommandPaste::UndoCommandPaste(QList<QList<QPixmap>> copiedPixmaps, BattleMap *battleMap, MasterScreenHandler *masterScreenHandler, QUndoCommand *parent) :
+    QUndoCommand(parent),
+    m_copiedPixmaps(copiedPixmaps),
+    m_battleMap(battleMap),
+    m_masterScreenHandler(masterScreenHandler)
+{
+}
+
+/*!
+ * \brief This function is the destructor of the class UndoCommandPaste.
+ */
+UndoCommandPaste::~UndoCommandPaste()
+{
+}
+
+/*!
+ * \brief This function undos this command.
+ */
+void UndoCommandPaste::undo()
+{
+    /* paste pixmaps of overwritten Battle Map squares to Battle Map */
+    m_battleMap->changeBattleMapSquarePixmaps(m_firstRowIdx, m_firstColumnIdx, m_overwrittenPixmaps);
+
+    /* apply pixmaps of overwritten Battle Map squares to graphics items of master screen handler */
+    m_masterScreenHandler->changeBattleMapSquarePixmaps(m_firstRowIdx, m_firstColumnIdx, m_overwrittenPixmaps.count(), m_overwrittenPixmaps.first().count());
+}
+
+/*!
+ * \brief This function applies or redos this command.
+ */
+void UndoCommandPaste::redo()
+{
+    /* determine indexes of row and column of the first Battle Map Square from selection area */
+    m_masterScreenHandler->getIndexesOfFirstBattleMapSquareFromSelectionArea(&m_firstRowIdx, &m_firstColumnIdx);
+
+    /* store pixmaps of Battle Map squares to be overwritten */
+    for (quint32 rowIdx = 0U; rowIdx < m_copiedPixmaps.count(); rowIdx++)
+    {
+        /* append new row of pixmaps */
+        m_overwrittenPixmaps.append(QList<QPixmap>());
+
+        for (quint32 columnIdx = 0U; columnIdx < m_copiedPixmaps.first().count(); columnIdx++)
+        {
+            /* append new pixmap to row */
+            if ((m_firstRowIdx + rowIdx < m_battleMap->getNumberRows()) && (m_firstColumnIdx + columnIdx < m_battleMap->getNumberColumns()))
+            {
+                m_overwrittenPixmaps.last().append(m_battleMap->getBattleMapSquarePixmap(m_firstRowIdx + rowIdx, m_firstColumnIdx + columnIdx));
+            }
+        }
+
+        /* remove row of pixmaps if no pixmaps have been appended */
+        if (0U == m_overwrittenPixmaps.last().count())
+        {
+            m_overwrittenPixmaps.removeLast();
+        }
+    }
+
+    /* paste pixmaps of copied Battle Map squares to Battle Map */
+    m_battleMap->changeBattleMapSquarePixmaps(m_firstRowIdx, m_firstColumnIdx, m_copiedPixmaps);
+
+    /* apply pixmaps of copied Battle Map squares to graphics items of master screen handler */
+    m_masterScreenHandler->changeBattleMapSquarePixmaps(m_firstRowIdx, m_firstColumnIdx, m_copiedPixmaps.count(), m_copiedPixmaps.first().count());
+}
+
+/****************************************************************************************************************************************************
+ * DEFINITION OF PROTECTED FUNCTIONS                                                                                                                *
+ ****************************************************************************************************************************************************/
+
+/* - */
+
+/****************************************************************************************************************************************************
+ * DEFINITION OF PRIVATE SLOT FUNCTIONS                                                                                                             *
+ ****************************************************************************************************************************************************/
+
+/* - */
+
+/****************************************************************************************************************************************************
+ * DEFINITION OF PRIVATE FUNCTIONS                                                                                                                  *
+ ****************************************************************************************************************************************************/
+
+/* - */
---
+++ undocommandpaste.h
@@ -0,0 +1,108 @@
+#ifndef UNDOCOMMANDPASTE_H
+#define UNDOCOMMANDPASTE_H
+
+/****************************************************************************************************************************************************
+ * INCLUDES                                                                                                                                         *
+ ****************************************************************************************************************************************************/
+
+#include "battlemap.h"
+#include "masterscreenhandler.h"
+
+#include <QUndoCommand>
+
+/****************************************************************************************************************************************************
+ * CLASS DECLARATION                                                                                                                                *
+ ****************************************************************************************************************************************************/
+
+/*!
+ * \brief This class implements the functionality needed for the pasting of the previously copied clipboard to the Battle Map and its undo mechanism.
+ */
+class UndoCommandPaste : public QUndoCommand
+{
+
+public:
+
+    /*! *********************************************************************************************************************************************
+     * \brief   This function is the constructor of the class UndoCommandPaste.                                                                     *
+     *                                                                                                                                              *
+     * \details -                                                                                                                                   *
+     *                                                                                                                                              *
+     * \param   copiedPixmaps                 Pixmaps of the copied Battle Map squares                                                              *
+     * \param   battleMap                     Address of the Battle Map                                                                             *
+     * \param   masterScreenHandler           Address of the master screen handler                                                                  *
+     * \param   parent                        Parent of the class UndoCommandPaste                                                                  *
+     *                                                                                                                                              *
+     * \return  This function does not have any return value.                                                                                       *
+     ************************************************************************************************************************************************/
+    UndoCommandPaste(QList<QList<QPixmap>> copiedPixmaps, BattleMap *battleMap, MasterScreenHandler *masterScreenHandler, QUndoCommand *parent = nullptr);
+
+    /*! *********************************************************************************************************************************************
+     * \brief   This function is the destructor of the class UndoCommandPaste.                                                                      *
+     *                                                                                                                                              *
+     * \details -                                                                                                                                   *
+     *                                                                                                                                              *
+     * \return  This function does not have any return value.                                                                                       *
+     ************************************************************************************************************************************************/
+    ~UndoCommandPaste();
+
+    /*! *********************************************************************************************************************************************
+     * \brief   This function undos this command.                                                                                                   *
+     *                                                                                                                                              *
+     * \details This function pastes the pixmaps of the overwritten Battle Map squares to the Battle Map and applies these pixmaps to the graphics  *
+     *          items of the master screen handler.                                                                                                 *
+     *                                                                                                                                              *
+     * \return  This function does not have any return value.                                                                                       *
+     ************************************************************************************************************************************************/
+    void undo() override;
+
+    /*! *********************************************************************************************************************************************
+     * \brief   This function applies or redos this command.                                                                                        *
+     *                                                                                                                                              *
+     * \details This function determines the indexes of the row and the column of the first Battle Map Square from the selection area. Afterwards,  *
+     *          it stores the pixmaps of the Battle Map squares to be overwritten. Finally, it pastes the pixmaps of the copied Battle Map squares  *
+     *          to the Battle Map and applies these pixmaps to the graphics items of the master screen handler.                                     *
+     *                                                                                                                                              *
+     * \return  This function does not have any return value.                                                                                       *
+     ************************************************************************************************************************************************/
+    void redo() override;
+
+protected: /* - */
+
+signals: /* - */
+
+private slots: /* - */
+
+private:
+
+    /*!
+     * \brief This is the variable containing the index of the first row where the paste action takes place.
+     */
+    quint32 m_firstRowIdx;
+
+    /*!
+     * \brief This is the variable containing the index of the first column where the paste action takes place.
+     */
+    quint32 m_firstColumnIdx;
+
+    /*!
+     * \brief This is the variable containing the pixmaps of the overwritten Battle Map squares.
+     */
+    QList<QList<QPixmap>> m_overwrittenPixmaps;
+
+    /*!
+     * \brief This is the variable containing the pixmaps of the copied Battle Map squares.
+     */
+    QList<QList<QPixmap>> m_copiedPixmaps;
+
+    /*!
+     * \brief This is a pointer to the Battle Map.
+     */
+    BattleMap *m_battleMap;
+
+    /*!
+     * \brief This is a pointer to the handler for displaying the Battle Map scene on the master screen.
+     */
+    MasterScreenHandler *m_masterScreenHandler;
+};
+
+#endif // UNDOCOMMANDPASTE_H
