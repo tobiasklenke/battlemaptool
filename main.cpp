9cff7eca6d7235dc122d630a2cd625eda3e7fdb3
Tobias Klenke <tobias.klenke@htp-tel.de>
2024-10-05 18:31:38 +0200

    implemented determination of master and player screen resolution and size values of screens associated with the windowing system the application is connected to

--- main.cpp
+++ main.cpp
@@ -8,10 +8,16 @@
 
 #include "defines.h"
 #include "mainwindow.h"
+#include "screencalc.h"
 
 /****************************************************************************************************************************************************
  * CONSTANTS                                                                                                                                        *
  ****************************************************************************************************************************************************/
+
+/*!
+ * \brief This is the name of the organization.
+ */
+const QString organizationName = QString("Battle Map Tool");
 
 /*!
  * \brief This is the name of the application.
@@ -22,6 +28,16 @@ const QString applicationName = QString("Battle Map Tool");
  * \brief This is the version of the application.
  */
 const QString applicationVersion = QString("0.9.1");
+
+/*!
+ * \brief This is the index of the master screen.
+ */
+const int masterScreenIndex = 0;
+
+/*!
+ * \brief This is the index of the player screen.
+ */
+const int playerScreenIndex = 1;
 
 /*!
  * \brief This is the period of time in milliseconds that the splash screens at application start are shown.
@@ -59,10 +75,32 @@ const int splashScreenFontSizeApplicationVersion = 12;
  */
 int main(int argc, char *argv[])
 {
-    /* initialize application with name and version, which are shown on the splash screens */
+    /* initialize application with organization name and also application name and version, which are shown on the splash screens */
     QApplication battleMapTool(argc, argv);
+    battleMapTool.setOrganizationName(organizationName);
     battleMapTool.setApplicationName(applicationName);
     battleMapTool.setApplicationVersion(applicationVersion);
+
+    /* determine master and player screen resolution and size values of screens associated with the windowing system the application is connected to */
+    int masterScreenResolutionWidth = QGuiApplication::screens().at(masterScreenIndex)->size().width();
+    int masterScreenResolutionHeight = QGuiApplication::screens().at(masterScreenIndex)->size().height();
+    int playerScreenResolutionWidth = QGuiApplication::screens().at(playerScreenIndex)->size().width();
+    int playerScreenResolutionHeight = QGuiApplication::screens().at(playerScreenIndex)->size().height();
+    int playerScreenSizeWidth = QGuiApplication::screens().at(playerScreenIndex)->physicalSize().width();
+    int playerScreenSizeHeight = QGuiApplication::screens().at(playerScreenIndex)->physicalSize().height();
+
+    /* check whether application is not yet initially configured */
+    QSettings settings;
+    if (!settings.value(CONFIGKEY_GENERAL_INITIALLY_CONFIGURED).toBool())
+    {
+        /* set configuration parameters of player screen resolution and diagonal size according to second screen associated with the windowing system the application is connected to */
+        settings.setValue(CONFIGKEY_PLAYERSCREEN_RESOLUTION_WIDTH, playerScreenResolutionWidth);
+        settings.setValue(CONFIGKEY_PLAYERSCREEN_RESOLUTION_HEIGHT, playerScreenResolutionHeight);
+        settings.setValue(CONFIGKEY_PLAYERSCREEN_DIAGONAL, roundToDecimalPlaces(calcScreenDiagonalInInches(playerScreenSizeWidth, playerScreenSizeHeight), PLAYERSCREEN_DIAGONAL_DECIMAL_PLACES));
+
+        /* set configuration parameter containing information whether application is initially configured */
+        settings.setValue(CONFIGKEY_GENERAL_INITIALLY_CONFIGURED, true);
+    }
 
     /* load splash screen image and draw text onto it containing application name and application version */
     QPixmap splashScreenPixmap(SPLASHSCREENIMAGE_SOURCE);
@@ -75,10 +113,10 @@ int main(int argc, char *argv[])
     /* show splash screens centrally on both main window and player window */
     QSplashScreen splashScreen1(splashScreenPixmap);
     QSplashScreen splashScreen2(splashScreenPixmap);
-    int splashScreen1PosX = (CONFIG_MASTER_SCREEN_RESOLUTION.width() - splashScreenPixmap.width()) / 2;
-    int splashScreen1PosY = (CONFIG_MASTER_SCREEN_RESOLUTION.height() - splashScreenPixmap.height()) / 2;
-    int splashScreen2PosX = (CONFIG_PLAYER_SCREEN_RESOLUTION.width() - splashScreenPixmap.width()) / 2 + CONFIG_MASTER_SCREEN_RESOLUTION.width();
-    int splashScreen2PosY = (CONFIG_PLAYER_SCREEN_RESOLUTION.height() - splashScreenPixmap.height()) / 2;
+    int splashScreen1PosX = (masterScreenResolutionWidth - splashScreenPixmap.width()) / 2;
+    int splashScreen1PosY = (masterScreenResolutionHeight - splashScreenPixmap.height()) / 2;
+    int splashScreen2PosX = (playerScreenResolutionWidth - splashScreenPixmap.width()) / 2 + masterScreenResolutionWidth;
+    int splashScreen2PosY = (playerScreenResolutionHeight - splashScreenPixmap.height()) / 2;
     splashScreen1.move(splashScreen1PosX, splashScreen1PosY);
     splashScreen2.move(splashScreen2PosX, splashScreen2PosY);
     splashScreen1.show();
@@ -96,7 +134,7 @@ int main(int argc, char *argv[])
 
     /* move main window and player window to respective screens */
     mainWindow.move(0, 0);
-    playerWindow.move(CONFIG_MASTER_SCREEN_RESOLUTION.width(), 0);
+    playerWindow.move(masterScreenResolutionWidth, 0);
 
     return battleMapTool.exec();
 }
